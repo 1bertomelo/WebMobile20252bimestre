da4a0501d957aef54f07f9a4bc99fa2614d152ec
betomelo_9 <betomelo_9@hotmail.com>
2025-05-08 16:50:03 -0300

    aula 08052025

---
+++ Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } 
+from '@react-navigation/stack';
+import { NavigationContainer } 
+from '@react-navigation/native';
+import Pagina1 from './src/Screens/Pagina1';
+import Pagina2 from './src/Screens/Pagina2';
+import HomeScreen from './src/Screens/HomeScreen';
+import JokeDetailScreen from './src/Screens/JokeDetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="Pagina1" component={Pagina1} />
+        <Stack.Screen name="Pagina2" component={Pagina2} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Piadas' }} />
+        <Stack.Screen name="JokeDetail" component={JokeDetailScreen} options={{ title: 'Detalhe da Piada' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
