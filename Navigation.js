4355cad99907008138ee1f70da91d04f6d4147e9
betomelo_9 <betomelo_9@hotmail.com>
2025-05-13 23:17:08 -0300

    splash

--- Navigation.js
+++ Navigation.js
@@ -7,13 +7,15 @@ import Pagina1 from './src/Screens/Pagina1';
 import Pagina2 from './src/Screens/Pagina2';
 import HomeScreen from './src/Screens/HomeScreen';
 import JokeDetailScreen from './src/Screens/JokeDetailScreen';
+import Splash from './src/Screens/splash';
 
 const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false }} />
         <Stack.Screen name="Pagina1" component={Pagina1} />
         <Stack.Screen name="Pagina2" component={Pagina2} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Piadas' }} />
