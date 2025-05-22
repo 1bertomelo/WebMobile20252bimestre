b16d9bb6de9462492ccce00042c04471e5c10e97
betomelo_9 <betomelo_9@hotmail.com>
2025-05-22 19:20:11 -0300

    aula 22-05

--- Navigation.js
+++ Navigation.js
@@ -8,14 +8,19 @@ import Pagina2 from './src/Screens/Pagina2';
 import HomeScreen from './src/Screens/HomeScreen';
 import JokeDetailScreen from './src/Screens/JokeDetailScreen';
 import Splash from './src/Screens/splash';
+import exemploSkeleton from './src/Screens/exemploSkeleton';
+import CadastroScreen from './src/Screens/CadastroScreen';
+import ListaScreen from './src/Screens/ListaScreen';
 
 const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={Splash} options={{headerShown: false }} />
+      <Stack.Navigator initialRouteName="exemploSkeleton">
+         <Stack.Screen name="exemploSkeleton" component={exemploSkeleton} options={{headerShown: false }} />
+        <Stack.Screen name="CadastroScreen" component={CadastroScreen} />
+        <Stack.Screen name="ListaScreen" component={ListaScreen} />
         <Stack.Screen name="Pagina1" component={Pagina1} />
         <Stack.Screen name="Pagina2" component={Pagina2} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Piadas' }} />
